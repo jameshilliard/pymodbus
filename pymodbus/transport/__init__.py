f67cc88588109c95663ede2f9af84509d522abc3
jan iversen <jancasacondor@gmail.com>
2024-01-13 16:07:29 +0100

    Prepare.

--- pymodbus/transport/__init__.py
+++ pymodbus/transport/__init__.py
@@ -1,11 +1,17 @@
 """Transport."""
 __all__ = [
+    "CommFrameType",
     "CommParams",
     "CommType",
+    "ModbusMessage",
     "ModbusProtocol",
     "NULLMODEM_HOST",
 ]
 
+from pymodbus.transport.message import (
+    CommFrameType,
+    ModbusMessage,
+)
 from pymodbus.transport.transport import (
     NULLMODEM_HOST,
     CommParams,
